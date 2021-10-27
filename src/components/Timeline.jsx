6f05a15bcb8d4470d9b958b377566beee2386225
D. Stuart Freeman <stuart.freeman@c21u.gatech.edu>
2021-10-27 18:51:04 -0400

    add button for reversing timeline order
    fixes #1

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import RITimeline from 'react-image-timeline';
 import 'react-image-timeline/dist/timeline.css';
 import '../styles/timeline.css';
@@ -6,12 +6,20 @@ import PropTypes from 'prop-types';
 
 const customFooter = (props) => <div/>;
 
-const Timeline = (props) => (
-  <RITimeline
-    events={props.events}
-    customComponents={{footer: customFooter}}
-  />
-);
+const Timeline = (props) => {
+  const [reverse, setReverse] = useState(false);
+
+  return <>
+    <button type="button" onClick={() => setReverse(!reverse)}>
+      Reverse Order
+    </button>
+    <RITimeline
+      events={props.events}
+      customComponents={{footer: customFooter}}
+      reverseOrder={reverse}
+    />
+  </>;
+};
 Timeline.propTypes = {
   events: PropTypes.object,
 };
